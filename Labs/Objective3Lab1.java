763c26136116c2e28f1fa596bdf0256476c1c5db
Leo Song <leosong21@gmail.com>
2021-09-08 16:26:50 -0400

    objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+  public static void main (String [] args) {
+    double num1 = 20;
+    double num2 = 30;
+    double num3 = 10;
+    double average = (num1 + num2 + num3)/3;
+      System.out.println(average);
+  }
+}
