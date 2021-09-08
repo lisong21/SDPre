e757b0cc8ac8e3efa0cd09507bdae16b91e3be50
Leo Song <leosong21@gmail.com>
2021-09-08 14:00:10 -0400

    objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Leo Song";
+    int age = 29;
+    String state = "Pennsylvania";
+      System.out.println("Name: " +  name +  " Age: "  +  age +  " State: " +  state);
+
+
+  }
+}
