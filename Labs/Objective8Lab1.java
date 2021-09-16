94e95d356d6d12ad9258e9aee91eaadb894e5886
Leo Song <leosong21@gmail.com>
2021-09-16 09:47:45 -0400

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int i=1; i<=10; i+=1) {
+      System.out.println(i);
+    }
+    for (int o=10; o>=1; o-=1) {
+      System.out.println(o);
+    }
+  }
+}
